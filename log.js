e7ca09bfbb4c58bb6f44e6bda5733629b8ac123f
sidharth_vinod <sidharthv96@gmail.com>
2017-12-31 12:55:08 +0530

    CDD

--- log.js
+++ log.js
@@ -1,9 +1,28 @@
 var Docker = require('dockerode');
 var fs     = require('fs');
 var stream = require('stream');
+
+
+const dgram = require('dgram')
+const split = require('split')
+const chrono = require('chrono-node')
+const JSON5 = require('json5')
+const yargs = require('yargs')
+const map = require('through2-map')
+
 var docker = new Docker({
   socketPath: '/var/hrun/docker.sock'
 });
+
+let isClosed = false
+let isSending = 0
+let socket = dgram.createSocket('udp4')
+
+
+socket.bind(function () {
+  socket.setBroadcast(true)
+})
+
 
 /**
  * Get logs from running container
@@ -12,8 +31,41 @@ function containerLogs(container) {
 
   // create a single stream for stdin and stdout
   var logStream = new stream.PassThrough();
-  logStream.on('data', function(chunk){
-    console.log(chunk.toString('utf8'));
+  let baseMessage = { id: container.name };
+  logStream.on('data', function (line) {
+    let timestamp = null;
+
+    try {
+      // try to JSON parse
+      line = JSON5.parse(line);
+    } catch (err) {
+      // look for timestamps if not an object
+      timestamp = argv.parseDate ? chrono.parse(line)[0] : null;
+    }
+
+    if (timestamp) {
+      // escape for regexp and remove from line
+      timestamp.text = timestamp.text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+      line = line.replace(new RegExp(' *[^ ]?' + timestamp.text + '[^ ]? *'), '');
+      // use timestamp as line timestamp
+      baseMessage.timestamp = Date.parse(timestamp.start.date());
+    } else {
+      baseMessage.timestamp = Date.now();
+    }
+
+    // update default message
+    baseMessage.content = line;
+
+    // prepare binary message
+    let buffer = new Buffer(JSON.stringify(baseMessage));
+
+    // set semaphore
+    isSending ++;
+
+    socket.send(buffer, 0, buffer.length, "9091", "localhost", function () {
+      isSending --;
+      if (isClosed && !isSending) socket.close();
+    })
   });
 
   container.logs({
@@ -29,17 +81,12 @@ function containerLogs(container) {
       logStream.end('!stop!');
     });
 
-    setTimeout(function() {
-      stream.destroy();
-    }, 2000);
   });
 }
 
-docker.createContainer({
-  Image: 'alpine',
-  Cmd: ['/bin/ash', '-c', 'ping 8.8.8.8']
-}, function(err, container) {
-  container.start({}, function(err, data) {
-    containerLogs(container);
-  });
+docker.listContainers({all: true}, function(err, containers) {
+    containers.forEach(function(data){
+        console.log(data);
+        containerLogs(data);
+    });
 });
