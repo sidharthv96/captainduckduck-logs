b97f3c89f19cf897787fbe1451a5c783ef2a0859
sidharth_vinod <sidharthv96@gmail.com>
2018-01-01 22:50:47 +0530

    CDD

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
-rtail-server --web-port 80 --web-host 0.0.0.0 --udp-host 0.0.0.0 --udp-port 9091
-# node log.js
+rtail-server --web-port 80 --web-host 0.0.0.0 --udp-host 0.0.0.0 --udp-port 9091 &
+node log.js
